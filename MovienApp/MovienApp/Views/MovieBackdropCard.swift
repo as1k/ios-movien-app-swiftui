7e7ccf06251220d19eca7974b630387e7d99f4a1
asikn <nurmukhambet.ak@gmail.com>
2021-05-12 11:15:34 +0600

    Feat: add image loader observable object

---
+++ MovienApp/MovienApp/Views/MovieBackdropCard.swift
@@ -0,0 +1,21 @@
+//
+//  MovieBackdropCard.swift
+//  MovienApp
+//
+//  Created by Асыланбек Нурмухамбет on 5/12/21.
+//  Copyright © 2021 kbtu.edu.as1k.kz. All rights reserved.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
